1a6a84e12185dddc9a79f848f18b83bd7921a647
andrew hulme <andrew_hulme04@hotmail.com>
2025-02-12 18:23:17 +1100

    Set up routes for all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import Home from "./pages/Home.jsx";
+import Games from "./pages/Games.jsx";
+import Software from "./pages/Software.jsx";
+import About from "./pages/About.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/games" element={<Games />} />
+        <Route path="/software" element={<Software />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   )
 }
 
