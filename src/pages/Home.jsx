8a3ce52cb516b6ad37b0b92598e717774ceff935
andrew hulme <andrew_hulme04@hotmail.com>
2025-02-27 11:20:40 +1100

    Fixed positioning of navbar and correctly aligned the whole page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Home = () => {
   return (
-    <>
+    <div className="container">
       <h1>MoonFall Software</h1>
       <h2>Text</h2>
       <h3>Home</h3>
-    </>
+    </div>
   )
 };
 
