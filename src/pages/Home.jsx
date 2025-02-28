a768ebca6fd4286c0c1ec9ac9676edeea06a7604
andrew hulme <andrew_hulme04@hotmail.com>
2025-02-28 15:29:24 +1100

    Added logo and tagline to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,13 @@ import React from "react";
 const Home = () => {
   return (
     <div className="container">
-      <h1>MoonFall Software</h1>
-      <h2>Home</h2>
+      <div className="logo-container">
+      <img src="/images/MoonFallLogo.png" alt="MoonFall Software Logo" className="logo"/>
+      </div>
+      <div className="heading-container">
+        <h2>Creativity | Quality | Passion</h2>
+        <p>Crafting unique game worlds to explore and powerful software to fuel your creativity, all with a commitment to quality.</p>
+      </div>
     </div>
   )
 };
