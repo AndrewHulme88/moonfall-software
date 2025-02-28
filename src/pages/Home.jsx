910dbb679cf9b5194af3276626ce2f9a648ae15b
andrew hulme <andrew_hulme04@hotmail.com>
2025-02-28 16:49:34 +1100

    Added games to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,13 @@ const Home = () => {
       </div>
       <div className="heading-container">
         <h2>Creativity | Quality | Passion</h2>
-        <p>Crafting unique game worlds to explore and powerful software to fuel your creativity, all with a commitment to quality.</p>
+        <p>Crafting unique game worlds to explore and reliable software to fuel your creativity, all with a commitment to quality.</p>
+      </div>
+      <div className="home-container-games">
+        <h2>MoonFall Software Games</h2>
+        <img src="/images/EliPoster.png" alt="EliPoster" className="home-game-image"/>
+        <p>CHeck out the original game jam version in your browser free:</p>
+        <a href="https://moonfallsoftware.itch.io/elis-odyssey-brackeysjam" className="game-link" target="_blank" rel="noopener noreferrer">Eli's Odyssey</a>
       </div>
     </div>
   )
